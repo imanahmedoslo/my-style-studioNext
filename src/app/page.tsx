f4ea3b001c7f49369295bccd905b55c6ac6f15ef
imanahmedoslo <imanahmedimanahmed@gmail.com>
2024-07-08 19:43:21 +0200

    will work with building the website and styling tomorrow, will watch some tutorials now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export default function Home() {
   return (
-   <div>hei</div>
+   <div>hei, jeg skal lese mer om tailwind og next js for å finne ut hvordan jeg kan sette prosjektet opp og kopiere styling fra ahmed sin prosjekt</div>
        
   );
 }
