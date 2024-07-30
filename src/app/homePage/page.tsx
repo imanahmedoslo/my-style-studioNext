844594a47d6e211cd6d6b40dcc8d654cb83b1026
Iman Ahmed <imanahmedimanahmed@gmail.com>
2024-07-30 22:29:44 +0200

    use an image link that works

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -10,15 +10,16 @@ import Carousel from '../components/carousel';
       </div>
       {/* TODO Create component */}
       <div className='w-full min-h-1/2 h-fit flex flex-row flex-wrap justify-center items-center bg-scroll'>
-        <ProductCard  title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
-        <ProductCard title="Sample Product" price={100} image="https://media.discordapp.net/attachments/1222505059468709901/1265996516225581117/customBlue-removebg-preview.png?ex=66a38aa2&is=66a23922&hm=b00c9d025a8472eaf7b7a50c33f55ca56dfba5c1561bb73f3ca85ae53a782a8b&=&format=webp&quality=lossless"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        <ProductCard  title="Sample Product" price={100} image="https://i.ibb.co/tBLDVnF/custom-Blue-removebg-preview.png"/>
+        
       </div>
     </div>
   );
